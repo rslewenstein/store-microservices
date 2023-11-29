54d6f6a823805e74a2550db1b0c47fb0417a1572
rslewenstein <rafasoaresjf@gmail.com>
2023-11-28 23:18:33 -0300

    FEAT: adding entities

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 - Project used to study about microservices.
 
 <img src="img/store.jpg">
+
+
+https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/?tabs=netcore-cli
+#
+https://learn.microsoft.com/pt-br/ef/core/providers/sqlite/?tabs=dotnet-core-cli
---
+++ WebApi.Products/Entity/Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Products.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public string? Type { get; set; }
+    }
+}
