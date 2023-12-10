cbd3d752c320db973bf8f081a99d43035d44a0e3
rslewenstein <rafasoaresjf@gmail.com>
2023-12-10 18:03:05 -0300

    FEAT: adding insert products scripts

---
+++ Util/Scripts/Insert_product.sql
@@ -0,0 +1,22 @@
+	INSERT INTO Products (ProductName, Type)
+	Values ('Camisa Manchester United', '2023');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Camisa Flamengo', 'Segundo uniforme - 2023');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Tenis Adidas', 'Runfalcon 3.0');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Corta Vento Puma', 'Black');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Jaqueta Adidas', 'White');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Jaqueta Adidas', 'Black');
+	
+	INSERT INTO Products (ProductName, Type)
+	Values ('Chuteira Puma King', 'Campo - Preta');
+	
+	select * from Products p ;
